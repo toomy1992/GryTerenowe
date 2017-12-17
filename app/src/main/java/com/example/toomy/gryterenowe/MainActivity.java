c3a1e883aa5a4305df83e4b70d53653b42c96e03
toomy1992 <toomy1992@gmail.com>
2017-12-17 01:33:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/toomy/gryterenowe/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.toomy.gryterenowe;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.Open,R.string.Close);
+        mDrawerlayout.addDrawerListener(mToggle);
+        NavigationView nvDrawer = (NavigationView) findViewById(R.id.nv);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupDrawerContent(nvDrawer);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(mToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void selectItemDrawer(MenuItem menuItem)
+    {
+        Fragment myFragment = null;
+        Class  fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.db:
+                fragmentClass = Graj.class;
+                break;
+            case R.id.account:
+                fragmentClass = Konto.class;
+                break;
+            case R.id.settings:
+                fragmentClass = Ustawienia.class;
+                break;
+            case R.id.help:
+                fragmentClass = Pomoc.class;
+                break;
+                default:
+                    fragmentClass = Graj.class;
+        }
+        try {
+            myFragment = (Fragment) fragmentClass.newInstance();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flcontent,myFragment).commit();
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawerlayout.closeDrawers();
+
+    }
+    private  void setupDrawerContent(NavigationView navigationView)
+    {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectItemDrawer(item);
+
+                return false;
+            }
+        });
+    }
+}
