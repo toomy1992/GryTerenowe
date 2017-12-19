f38c810a3eb908c104938077ea324a66d63ed30a
noemi <noemi@localhost.localdomain>
2017-12-20 00:13:00 +0100

    add blockade to next view when task is not finished

--- app/src/main/java/com/example/toomy/gryterenowe/QuestForUeCnti2.java
+++ app/src/main/java/com/example/toomy/gryterenowe/QuestForUeCnti2.java
@@ -1,20 +1,24 @@
 package com.example.toomy.gryterenowe;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-
+import android.widget.EditText;
+import android.text.TextWatcher;
+import android.graphics.Color;
 /**
  * A simple {@link Fragment} subclass.
  */
 public class QuestForUeCnti2 extends Fragment {
 
+
+    private EditText answer;
+    private Button QuestForUeBtn;
 
     public QuestForUeCnti2() {
         // Required empty public constructor
@@ -26,7 +30,13 @@ public class QuestForUeCnti2 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_quest_for_ue_cnti2, container, false);
-        Button QuestForUeBtn = (Button) v.findViewById(R.id.dalejcnti3);
+
+        QuestForUeBtn = (Button) v.findViewById(R.id.dalejcnti3);
+
+        answer = (EditText)v.findViewById(R.id.editText);
+        answer.addTextChangedListener(mWatcher);
+        QuestForUeBtn.setEnabled(false);
+
         QuestForUeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,5 +48,26 @@ public class QuestForUeCnti2 extends Fragment {
         });
         return v;
     }
+
+    private TextWatcher mWatcher = new TextWatcher() {
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            enableSubmitIfReady(answer, QuestForUeBtn);
+        }
+    };
+
+
+    public void enableSubmitIfReady(EditText answear, Button next) {
+
+        boolean isReady = answer.getText().toString().equals("@");
+        next.setEnabled(isReady);
+    }
+
 
 }
--- app/src/main/java/com/example/toomy/gryterenowe/QuestForUeCnti3.java
+++ app/src/main/java/com/example/toomy/gryterenowe/QuestForUeCnti3.java
@@ -4,17 +4,21 @@ package com.example.toomy.gryterenowe;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class QuestForUeCnti3 extends Fragment {
-
+    EditText answer;
+    Button QuestForUeBtn;
 
     public QuestForUeCnti3() {
         // Required empty public constructor
@@ -26,7 +30,12 @@ public class QuestForUeCnti3 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_quest_for_ue_cnti3, container, false);
-        Button QuestForUeBtn = (Button) v.findViewById(R.id.dalejcnti4);
+        QuestForUeBtn = (Button) v.findViewById(R.id.dalejcnti4);
+
+        answer = (EditText)v.findViewById(R.id.answer);
+        answer.addTextChangedListener(mWatcher);
+        QuestForUeBtn.setEnabled(false);
+
         QuestForUeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,5 +47,26 @@ public class QuestForUeCnti3 extends Fragment {
         });
         return v;
     }
+    private TextWatcher mWatcher = new TextWatcher() {
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            enableSubmitIfReady(answer, QuestForUeBtn);
+        }
+    };
+
+
+    public void enableSubmitIfReady(EditText answer, Button next) {
+
+        if (answer.getText().toString().equals("CNTI")){
+            next.setEnabled(true);
+        }
+    }
+
 
 }
